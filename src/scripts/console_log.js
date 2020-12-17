1bd45534b5ce47636772869c4f114fadb42022c5
Mark Howe <mvahowe@gmail.com>
2020-12-17 16:14:55 +0100

    Refactoring

---
+++ src/scripts/console_log.js
@@ -0,0 +1,36 @@
+const fse = require('fs-extra');
+const path = require('path');
+
+const {ProsKomma} = require('proskomma');
+const doModelQuery = require('../model_query');
+const renderModel = require('../scripture_para_result_model');
+const tokenActions = require("../action_classes/token/consoleLog");
+
+const fp = process.argv[2];
+const lang = process.argv[3];
+const abbr = process.argv[4];
+
+const content = fse.readFileSync(path.resolve(__dirname, fp));
+const contentType = fp.split('.').pop();
+const pk = new ProsKomma();
+pk.importDocument(
+    {lang, abbr},
+    contentType,
+    content,
+    {}
+);
+
+const doRender = async pk => {
+    await doModelQuery(pk)
+        .then(result => {
+                const model = new renderModel(result);
+                model.render({
+                    actions: {
+                        token: tokenActions
+                    }
+                });
+            }
+        )
+};
+
+doRender(pk).then(() => {console.log()});
--- src/test/code/hello.js
+++ src/test/code/hello.js
@@ -11,8 +11,8 @@ const scopeActions = require("../../action_classes/scope/chapterVerse");
 
 const testGroup = "The Basics";
 
-const pk = pkWithDoc("../test_data/rut.usfm", {lang: "eng", abbr: "web"})[0];
-// const pk = pkWithDoc("../../../../sbf/nfc/usx/040MAT.usx", {lang: "fra", abbr: "nfc"})[0];
+// const pk = pkWithDoc("../test_data/rut.usfm", {lang: "eng", abbr: "web"})[0];
+const pk = pkWithDoc("../../../../../sbf/nfc/usx/040MAT.usx", {lang: "fra", abbr: "nfc"})[0];
 
 const renderHeading = {
     test: (context, data) => {
